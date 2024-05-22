5bd5e92b706baa832b5691f45f739f1d32cb6c87
karinakasyan4 <120644243+karinakasyan4@users.noreply.github.com>
2024-05-22 08:45:54 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { MyMeals } from "./MyMeals";
+import { addMeal, getAllMeals,  editMeal, deleteMeal} from "./FetchMeals";
+
+
+function App() {
+
+  const [myMeal, setMeal] = useState([]);
+  const [title, setTitle] = useState("");
+  const [editing, setEditing] = useState(false);
+  const [mealId, setMealId] = useState("");
+
+  useEffect(() => {
+    getAllMeals(setMeal)
+  }, [])
+
+  const updatingInInput = (_id, title) => {
+    setEditing(true)
+    setTitle(title)
+    setMealId(_id)
+  }
+
+  return (
+    <div>
+    <h1>Meal Plan</h1>
+    <input
+    type='text'
+    placeholder='Add you meal..'
+    value={title}
+    onChange={(e) => setTitle(e.target.value)}/>
+
+    <button
+    disabled={!title}
+    onClick={
+      editing ? () => editMeal(mealId, title, setTitle, setMeal, setEditing) :
+      () => addMeal(title, setTitle, setMeal)}>
+     {editing ? 'Edit' : 'Add'}
+    </button>
+
+{myMeal.map((meal) => <MyMeals text={meal.title} key={meal._id} 
+updatingInInput={()=>updatingInInput(meal._id, meal.title)}
+deleteMeal={()=>deleteMeal(meal._id, setMeal)}/>)}
+{/* <MyMeals text={'we got here'}/> */}
+
+    </div>
+  
+  
+  )
+}
+
+export default App;
